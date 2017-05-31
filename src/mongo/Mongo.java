dae81ea714038597021890c8b5e93c9f117e4ee4
Iago Lopez Fernandez <ilopez@iesjoandaustria.org>
2017-05-31 16:21:37 +0200

    Limpieza

--- src/mongo/Mongo.java
+++ src/mongo/Mongo.java
@@ -1,14 +1,5 @@
 package mongo;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import org.bson.Document;
-
 public class Mongo {
 
     public static void main(String[] args) {
